3d9d98b9a6ecb220cf685182b6f5092990ff5f95
Jamie Archibald <jamie.archibald@westjet.com>
2017-07-12 21:23:17 -0600

    initial commit. sqlite database working with rest api.

---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Boom = require('boom');
+const Wreck = require('wreck');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Readable = require('stream').Readable;
+const Pack = require('./package');
+
+const username = process.env.SABRE_USERNAME;
+const password = process.env.SABRE_PASSWORD;
+const sabreUrl = process.env.SABRE_URL;
+const port = process.env.PORT || 9000;
+
+const server = new Hapi.Server();
+
+server.connection({
+    host: 'localhost',
+    port: port
+});
+
+const options = {
+  info: {
+    'title': 'Node Books',
+    'version': Pack.version,
+  }
+};
+
+server.register([
+  Inert,
+  Vision,
+  {
+    'register': HapiSwagger,
+    'options': options
+}, {
+    register: require('./plugins/tasks/index.js'),
+    options: {}
+}], (err) => {
+
+    if (err) {
+        throw err;m
+    }
+});
+
+// Start the server
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
---
+++ plugins/tasks/index.js
@@ -0,0 +1,118 @@
+'use strict';
+
+const Boom = require('boom');
+const Joi = require('joi');
+const sqlite3 = require('sqlite3').verbose();
+const dbFile = 'db.txt'
+
+const wreckOptions = {
+    headers: {
+        'content-type': 'text/xml'
+    },
+    rejectUnauthorized: true
+};
+
+
+exports.register = function(server, options, next) {
+
+  const db = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE);
+
+  db.serialize(function() {
+    //db.run("DROP TABLE records");
+   db.run("CREATE TABLE IF NOT EXISTS records (date DATE, folder TEXT, client TEXT, file TEXT, account TEXT, category TEXT, notes TEXT, hours NUMERIC, rate NUMERIC)");
+   db.close();
+  });
+
+  server.route({
+      method: 'POST',
+      path:'/records',
+      config: {
+        handler: createRecord,
+        description: 'Creates a new record',
+        notes: ['Test'],
+        tags: ['api'],
+        validate: {
+          payload: {
+            date: Joi.string().required(),//regex(/^[0-9]{4}[0-9]{2}[0-9]{2}$/).required(),
+            folder: Joi.string().required(),
+            client: Joi.string().required(),
+            file: Joi.string().optional(),
+            account: Joi.string().optional(),
+            category: Joi.string().required(),
+            notes: Joi.string().optional(),
+            hours: Joi.number().min(0).required(),
+            rate: Joi.number().min(0).required()
+          }
+        }
+      }
+  });
+
+  server.route({
+      method: 'GET',
+      path:'/records',
+      config: {
+        handler: getRecords,
+        description: 'Gets all the records',
+        notes: ['Test'],
+        tags: ['api']
+        //,
+        /*validate: {
+          payload: {
+            folder: Joi.string().required().description('folder required')
+          }
+        }*/
+      }
+  });
+
+  next();
+};
+
+const createRecord = function (request, reply) {
+
+  console.log('creating record');
+
+  var record = {
+    date: request.payload.date,
+    folder: request.payload.folder,
+    client: request.payload.client,
+    file: request.payload.file,
+    account: request.payload.account,
+    category: request.payload.category,
+    notes: request.payload.notes,
+    hours: request.payload.hours,
+    rate: request.payload.rate
+  };
+
+  const db = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE);
+  db.serialize(function() {
+    var stmt = db.prepare("INSERT INTO records (date, folder, client, file, account, category, notes, hours, rate) VALUES (?,?,?,?,?,?,?,?,?)");
+    stmt.run(record.date, record.folder, record.client, record.file, record.account, record.category, record.notes, record.hours, record.rate);
+    stmt.finalize();
+    db.close();
+    return reply(record);
+  });
+}
+
+const getRecords = function (request, reply) {
+
+  console.log('get records');
+
+  const db = new sqlite3.Database(dbFile, sqlite3.OPEN_READONLY);
+
+  db.serialize(function() {
+    db.all("SELECT * FROM records", function(err, rows) {
+      if (err) {
+        console.error(err);
+        reply(Boom.badImplementation());
+      } else {
+        reply(rows);
+      }
+      db.close();
+    });
+  });
+
+}
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
